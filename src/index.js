caba02337262703464ebff7126dd32957c63d54f
Andrés Pardo Rodríguez <apardor@gmail.com>
2022-10-20 11:56:32 +0200

    search params fetching image. next: creating multiple image cards

--- src/index.js
+++ src/index.js
@@ -1,59 +1,62 @@
 import example from '../images/githubFooter.svg'
 import './styles/main.scss'
 
-const header = document.createElement('header')
+let state = {
+  logo: 'Logo!',
+  search: ''
+};
+let count = window.history.state?.count || 0;
+const template = (state) => {
+  if (state) {
+    return `
+    <header>
+    <nav>
+      <img src="" alt="" class="logo"/>
+      <h1>${state.logo}</h1>
+    </nav>
+    </header>
+    <main>
+      <section>
+        <form action="/"><input onchange="searchFunc(event)" type="text">
+        </form>
+      </section>
+      <section>
+        <article>
+          <img src=${state.search} alt="">
+        </article>
+      </section>
+    </main>
+    <footer>
+      <img src=${example} alt="github logo">
+    </footer>
+        `;
+  }
+};
+const render = (htmlString, el) => {
+  el.innerHTML = htmlString;
+};
+const update = (newState) => {
+  window.history.pushState(
+    {  ...state, ...newState },
+    "HISTORY",
+    `index.html#${count}`
+),
+  window.dispatchEvent(new Event("statechange"));
+};
+window.addEventListener("statechange", (e) => {
+  render(template(window.history.state), document.querySelector("#root"));
+});
 
-const nav = document.createElement('nav');
-header.append(nav);
+// update();
 
-const logo = document.createElement('h1')
-logo.textContent = 'LOGO!';
-nav.append(logo);
+render(template(window.history.state), document.querySelector("#root"));
 
-const main = document.createElement('main');
-const section = document.createElement('section');
-
-const form = document.createElement('form');
-form.setAttribute('action', '/');
-section.append(form);
-
-const input = document.createElement('input');
-input.setAttribute('id', 'contentSearch');
-input.setAttribute('type', 'text');
-form.append(input);
-
-
-const imageSection = document.createElement('section');
-imageSection.setAttribute('class', 'image-section');
-main.append(section, imageSection);
-
-
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-const footer = document.createElement('footer');
-const svgFooter = document.createElement('img');
-svgFooter.setAttribute('src', example)
-svgFooter.setAttribute('alt', 'github logo');
-footer.append(svgFooter);
-app.append(header, main, footer)
-
-const retrieveData = async () =>{
-  const response = await fetch('https://api.unsplash.com/search/photos?query=london&client_id=VuZcp_-aJVs8boOKxDfB7l4toXLboZFWW7jPl2Abbpk');
+const retrieveData = async (query) =>{
+  const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=VuZcp_-aJVs8boOKxDfB7l4toXLboZFWW7jPl2Abbpk`);
   const data = await response.json();
   return data;
 }
 
-const imageGrid = image => {
-  const article = document.createElement('article');
-  const img = document.createElement('img');
-  article.append(img)
-  img.setAttribute('src', image)
-  img.setAttribute('alt', 'article image');
-  imageSection.append(article)
-}
-
-retrieveData().then((data) => {
-  data.results.forEach(el => imageGrid(el.urls.thumb));
-})
-
+window.searchFunc = function (event){
+  retrieveData(event.target.value).then(data =>  data.results.forEach(el => update({search: el.urls.thumb})))
+ }
